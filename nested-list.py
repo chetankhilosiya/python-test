26a89175cc855ccd3932f4cb5c19e626ca3f8a5c
Chetan Khilosiya <chetan.khilosiya@gmail.com>
2018-09-17 12:04:37 +0530

    Dev library for refactoring and nested list solution. The dev library is required by vs code.

--- nested-list.py
+++ nested-list.py
@@ -4,4 +4,18 @@ if __name__ == '__main__':
         name = input()
         score = float(input())
         students.append([name, score])
-    print(students)
+    
+    bottom = 101
+    secondLowest = 101
+    for s in students:
+        if s[1] < bottom:
+            secondLowest = bottom
+            bottom = s[1]
+
+        elif s[1] < secondLowest and s[1] > bottom:
+            secondLowest = s[1]
+
+    ans = sorted([s[0] for s in students if s[1] == secondLowest])
+    for name in ans:
+        print(name)
+    
