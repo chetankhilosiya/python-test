26214762a5763e6858786b934199890e19d1bf58
Chetan Khilosiya <chetan.khilosiya@gmail.com>
2018-11-14 14:35:06 +0530

    Added string formatting problem solution.

--- string-formatting.py
+++ string-formatting.py
@@ -1,6 +1,7 @@
 def print_formatted(number):
-    # your code goes here
-    print("Hello", number)
+    max_length = len("{0:b}".format(number))
+    for i in range(1, number + 1):
+        print("{0:>{1}} {0:>{1}o} {0:>{1}X} {0:>{1}b}".format(i, max_length))
 
 if __name__ == '__main__':
     n = int(input())
